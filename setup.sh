cb0cf7c1620755b29b7f244914933f5223341758
Mauvis Ledford <mauvis@brainswap.me>
2011-10-16 18:08:41 +0000

    Start of a setup file.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+DIR=`pwd` #Dotfiles folder
+
+#echo $DIR
+cd ~/
+ln -is $DIR/.bash_profile .bash_profile
+ln -is $DIR/.ssh/config .ssh/config
+ln -is $DIR/.emacs .emacs
+ln -is $DIR/.gitconfig .gitconfig
+ln -is $DIR/site-lisp .emacs.d/site-lisp
+
+source ~/.bash_profile
+cd $DIR
