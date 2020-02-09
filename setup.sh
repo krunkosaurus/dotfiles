94abded417a3196040a750cef9e87a27d9222237
Mauvis Ledford <mauvis@multi.io>
2020-02-09 13:04:59 +0000

    ubuntu support and other updates

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
-#! /bin/sh
+#!/bin/bash
 DIR=`pwd` #Dotfiles folder
 
 #echo $DIR
 cd ~/
-#ln -is $DIR/.bash_profile .bash_profile
+ln -is $DIR/.bash_profile .bash_profile
 #ln -is $DIR/.ssh/config .ssh/config
 ln -is $DIR/.emacs .emacs
 ln -is $DIR/.gitconfig .gitconfig
-ln -is $DIR/site-lisp .emacs.d/site-lisp
+ln -is $DIR/.path .path
+ln -is $DIR/.bash_prompt .bash_prompt
+ln -is $DIR/.export .export
+ln -is $DIR/.alias .alias
+ln -is $DIR/.functions .functions
+ln -is $DIR/.emacs.d/site-lisp .emacs.d/site-lisp
 
-source ~/.bash_profile
+. ~/.bash_profile
 cd $DIR
 
-git submodules init
-git submodules update
+git submodule init
+git submodule update
